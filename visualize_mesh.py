e0e85614b8b7d58b880a42eb250e4cfb40d95741
Rishi Chandna <rishirules01@gmail.com>
2023-04-12 20:18:09 -0500

    reduce points for testing, normalize triangle norm to avoid near-zero issues

--- visualize_mesh.py
+++ visualize_mesh.py
@@ -15,7 +15,7 @@ mesh.compute_vertex_normals()
 
 
 # sample a point cloud from the mesh to work with
-pointcloud = mesh.sample_points_uniformly(number_of_points=2500)
+pointcloud = mesh.sample_points_uniformly(number_of_points=50)
 # o3d.visualization.draw_geometries([pointcloud], point_show_normal=True)
 
 PointsNP = np.asarray(pointcloud.points)
@@ -27,29 +27,31 @@ kd = o3d.geometry.KDTreeFlann(pointcloud)
 seed_point_bool_dict = dict.fromkeys(range(0, len(PointsNP)), True)
 
 
-def GetCenterOfBall(triangle_norm, sigma, s_a, s_b, radius):
+def GetCenterOfBall(triangle_norm, p1, p2, p3, radius):
     # triangle_norm is np.cross(p21, p31)
     # p1 is sigma
     # p2 is s_a
     # p3 is s_b
-    p21 = PointsNP[s_a] - sigma
-    p31 = PointsNP[s_b] - sigma
+    p21 = p2 - p1
+    p31 = p3 - p1
 
     circumcenter = np.cross(
         np.dot(p21, p21) * p31 - np.dot(p31, p31) * p21,
-        triangle_norm
-        ) / 2 / np.dot(triangle_norm, triangle_norm) + sigma
+        triangle_norm) / 2 / np.dot(triangle_norm, triangle_norm) + p1
 
-    t1 = np.sqrt((radius**2 - np.dot(circumcenter-sigma, circumcenter-sigma))/ np.dot(triangle_norm, triangle_norm))
+    # print(f"The circumcenter is {circumcenter} and the point p1 is {p1}, the dot product of the difference is {np.dot(circumcenter-p1, circumcenter-p1)}")
+    t1 = np.sqrt((radius**2 - np.dot(circumcenter-p1, circumcenter-p1))/ np.dot(triangle_norm, triangle_norm))
     center_of_ball = circumcenter + triangle_norm * t1
     return center_of_ball
 
 # is the triangle norm consistent with the point normals?
-def TestTriangleNorm(sigma, s_a, s_b):
-    triangle_norm = np.cross(PointsNP[s_a] - sigma, PointsNP[s_b] - sigma)
-    if (np.dot(triangle_norm, sigma) < 0):
+def TestTriangleNorm(sigma_idx, s_a, s_b):
+    triangle_norm = np.cross(PointsNP[s_a] - PointsNP[sigma_idx], PointsNP[s_b] - PointsNP[sigma_idx])
+    if (np.dot(triangle_norm, NormalsNP[sigma_idx]) < 0):
         triangle_norm *= -1
-    if (np.dot(triangle_norm, PointsNP[s_a]) > 0) and (np.dot(triangle_norm, PointsNP[s_b]) > 0):
+    if (np.dot(triangle_norm, NormalsNP[s_a]) > 0) and (np.dot(triangle_norm, NormalsNP[s_b]) > 0):
+        # print(f"found valid triangle norm for points p1 {PointsNP[sigma_idx]}|{NormalsNP[sigma_idx]}, p2 {PointsNP[s_a]}|{NormalsNP[s_a]}, p3{PointsNP[s_b]}|{NormalsNP[s_b]}")
+        triangle_norm /= np.linalg.norm(triangle_norm)
         return True, triangle_norm
     return False, None
 
@@ -60,8 +62,8 @@ def TestTriangleBall(triangle_norm, p1, p2, p3, radius):
         return False
     return True
 
-def TrySeed(sigma_idx, sigma, radius):
-    num_results, index_vec, distance2_vec = kd.search_radius_vector_3d(np.array(sigma), radius * 2)
+def TrySeed(sigma_idx, radius):
+    num_results, index_vec, distance2_vec = kd.search_radius_vector_3d(np.array(PointsNP[sigma_idx]), radius * 2)
     if (num_results < 3):
         return False
     # consider all sigma_a, sigma_b pairs
@@ -73,8 +75,8 @@ def TrySeed(sigma_idx, sigma, radius):
             if (s_b == s_a) or (s_b == sigma_idx) or (s_b in orphans):
                 continue
             # is the triangle norm consistent with the point normals?
-            tnorm_is_valid, triangle_norm = TestTriangleNorm(sigma, s_a, s_b)
-            if tnorm_is_valid and TestTriangleBall(triangle_norm, sigma, s_a, s_b, radius):
+            tnorm_is_valid, triangle_norm = TestTriangleNorm(sigma_idx, s_a, s_b)
+            if tnorm_is_valid and TestTriangleBall(triangle_norm, PointsNP[sigma_idx], PointsNP[s_a], PointsNP[s_b], radius):
                 # good seed
                 return True
     return False
@@ -84,7 +86,7 @@ def FindSeedTriangle(radius):
     possible_seed_indices = [k for k, v in seed_point_bool_dict.items() if v]
     for pidx in possible_seed_indices:
         if pidx not in orphans:
-            if TrySeed(pidx, PointsNP[pidx], radius):
+            if TrySeed(pidx, radius):
                 return pidx
     # no good seed, terminate algorithm
     return False
